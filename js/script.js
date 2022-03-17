c1409e09a1fead79e524fff997b683b4f95f4847
Max Rytik <max777dan@gmail.com>
2022-03-17 22:07:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '');
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+
+document.write(answers);
